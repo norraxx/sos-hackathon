89863ab6de3d9f6f0b6d7b785e9467269ef42d7c
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2018-09-21 18:14:18 +0200

    #! WIP

--- runner.py
+++ runner.py
@@ -1,11 +1,40 @@
+import subprocess
+from typing import List
+import sys
 # 1. nacitat mapu
 # 2. spustit skript se vstupem a precist vystup
 # 3. zvalidovat vystup
 # 4. provest vystup, vyhodnotit a upravit mapu
 # 5. ulozit mezivysledek nekam, at' vime co se deje
 
+bot_colors = ["A", "B", "C", "D"]
+
 
 def read_map(file_name):
+    # type: (str) -> List[str]
     with open(file_name, "r") as f:
-        return map(str.strip, f.read().split("\n"))
+        return [row.strip() for row in f.readline()]
 
+
+def run_map(map, bot):
+    pass
+
+
+def start():
+    map_name = sys.argv[1]
+    bot_names = sys.argv[2:]
+
+    map_ = read_map(map_name)
+    step_count = len(bot_names) * 50
+    for i, bot in enumerate(bot_names):
+        robot_color = bot_colors[i]
+        with open(f"{bot}/run.txt", "r") as f:
+            run_command = f.readline().strip().split()
+        p = subprocess.Popen(run_command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+        robot_action = p.communicate(input=map_ + "\n")[0]
+
+        validate(map_, robot_color, robot_action)
+
+
+if __name__ == "__main__":
+    start()
