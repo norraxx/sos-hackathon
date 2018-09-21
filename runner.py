ec115457c098f97048b7311b076ba24dbd89da25
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2018-09-21 17:42:43 +0200

    nastrel sracek, co musi umet spoutedlo

---
+++ runner.py
@@ -0,0 +1,5 @@
+# 1. nacitat mapu
+# 2. spustit skript se vstupem a precist vystup
+# 3. zvalidovat vystup
+# 4. provest vystup, vyhodnotit a upravit mapu
+# 5. ulozit mezivysledek nekam, at' vime co se deje
