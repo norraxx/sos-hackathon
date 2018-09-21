40a1e460515b7b96db22e4aa2232771d77e87202
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2018-09-21 17:48:48 +0200

    #! mapread

--- runner.py
+++ runner.py
@@ -3,3 +3,9 @@
 # 3. zvalidovat vystup
 # 4. provest vystup, vyhodnotit a upravit mapu
 # 5. ulozit mezivysledek nekam, at' vime co se deje
+
+
+def read_map(file_name):
+    with open(file_name, "r") as f:
+        return map(str.strip, f.read().split("\n"))
+
